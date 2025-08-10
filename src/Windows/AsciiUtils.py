7323ba00a0fe56e783cb4b10fd9d1da646e550b5
Two2Fall <166918540+Two2Fall@users.noreply.github.com>
2025-08-10 12:55:54 -0400

    Finished the TextSection() class in this file

--- src/Windows/AsciiUtils.py
+++ src/Windows/AsciiUtils.py
@@ -9,7 +9,7 @@ Used modules:
 
 from rich.console import Console
 
-AUTHOR = "TwellEvans"
+AUTHOR = "TwellEvans"  # Do not change this
 VERSION = 1.0
 
 
@@ -27,18 +27,64 @@ class TextSection:
     """
 
     def __init__(self, TabAmount: tuple[str, int], Console: Console) -> None:
-        self.Text = []
+        self.Paragraphs = []
         self.TabAmount = TabAmount
         self.Console = Console
+
     def AppendParagraph(self, Paragraph: str):
         """
-        TextSection().AppendParagraph()         —— A class for text sections
+        `TextSection().AppendParagraph()`         —— A method to append paragraphs
 
-        This class provides methods for text sections in the document, this text
-        can be stylized without any problem, because this class use the rich module
+        This method appends a paragraph into the text section, it can have style, so
+        you can personalize the text as you want.
 
         Arguments:
-            `Indentation` — The indentation symbol and amount of this document,
-            `Console`: — To be better compatible, this class needs a console to render the text section
-        Returns: `TextSection()`
+            `Paragraph` — The paragraph to append
+        Returns: `None`
         """
+        self.Paragraphs.append(Paragraph)
+
+    def RenderAll(self):
+        """
+        `TextSection().RenderAll()`         —— A method to render the text section
+
+        This method renders the entire text section.
+
+        Arguments: `None`
+        Returns: `None`
+        """
+        for Paragraph in self.Paragraphs:
+            self.Console.print(self.TabAmount + Paragraph)
+
+    def GetParagraph(self, ParagraphIndex: int | tuple[int, int]):
+        """
+        `TextSection().GetParagraph()`      —— A method to get a paragraph by index
+
+        If you need to get a paragraph by index
+        use this method.
+
+        Arguments:
+            `ParagraphIndex` — The paragraph index
+        Returns: `str` or `list`
+        """
+        if isinstance(ParagraphIndex, tuple):
+            return self.Paragraphs[ParagraphIndex[0] : ParagraphIndex[1]]
+        elif (
+            isinstance(ParagraphIndex, int)
+            and ParagraphIndex >= 0
+            and ParagraphIndex < len(self.Paragraphs)
+        ):
+            return self.Paragraphs[ParagraphIndex]
+
+    def RenderPart(self, Indexes: tuple[int, int]):
+        """
+        `TextSection().RenderPart()`      —— A method to render paragraphs by indexes
+
+        This method is to render a part of the ascii document. This is good if you want to
+        import quotes or any important text instead of importing all document
+
+        Arguments:
+            `Indexes` — This parameter is for the extracted text document (line by line)
+        Returns: `list`
+        """
+        return self.Paragraphs[Indexes[0] : Indexes[1]]
