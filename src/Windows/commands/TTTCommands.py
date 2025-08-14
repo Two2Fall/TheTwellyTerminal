fd1836d3f546f4df0c22fcb1e0819da28252a371
ameen272 <107958859+ameen272@users.noreply.github.com>
2025-08-15 02:19:06 +0300

    Update TTTCommands.py

--- src/Windows/commands/TTTCommands.py
+++ src/Windows/commands/TTTCommands.py
@@ -1,6 +1,6 @@
 def TTTCommands():
     """
-    TTTCommands()        -- Function The Twelly Creations.
+    TTTCommands()        -- Function that prints help related to The Twelly Creations.
     """
 
     print(" " * 19, "ttt  - The Twelly Tracker's command (Not implemented yet).")
