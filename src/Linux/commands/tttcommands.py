e32385ce23e25b4193c6b3f1f39544a336e2ec03
ameen272 <107958859+ameen272@users.noreply.github.com>
2025-08-15 02:09:00 +0300

    Update tttcommands.py

--- src/Linux/commands/tttcommands.py
+++ src/Linux/commands/tttcommands.py
@@ -3,6 +3,6 @@ def TTTCommands():
     TTTCommands()        -- Function that prints out help related to The Twelly Creations.
     """
 
-    print("ttt  - The Twelly Tracker's command (Not implemented yet).")
+    print(" " * 19, "ttt  - The Twelly Tracker's command (Not implemented yet).")
 
 TTTCommands()
