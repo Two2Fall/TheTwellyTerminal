5a53f01a2c4bd7b97f1d3b19b055457a7571149e
ameen272 <107958859+ameen272@users.noreply.github.com>
2025-08-15 02:04:14 +0300

    Update AsciiUtils.py

--- src/Linux/AsciiUtils.py
+++ src/Linux/AsciiUtils.py
@@ -5,9 +5,11 @@ This is a python file to create paragraphs and ascii documents better. This
 file includes the `TextSection()` class and the pages.
 Used modules:
     - rich
+    - typing
 """
 
 from rich.console import Console
+from typing import Union, Literal
 
 AUTHOR = "TwellEvans"  # Do not change this
 VERSION = 1.0
@@ -88,3 +90,83 @@ class TextSection:
         Returns: `list`
         """
         return self.Paragraphs[Indexes[0] : Indexes[1]]
+
+
+# Alright, so if I finished the TextSection() class
+# The next step, is the ascii document class
+
+
+class Document:
+    """
+    Document()         —— A class for an entire ascii document
+
+    This class is created for ascii documents, with title, header, contents,
+    indentation, sections, and more. This can import, export and render `asciidoc` files.
+
+    Arguments:
+        `DocumentName`: This parameter denotes the ascii document name
+        `DocumentAuthor`: This parameter denotes the author of the ascii document
+    Returns: `Document()`
+    """
+
+    def __init__(self, DocumentName: str, DocumentAuthor: str) -> None:
+        self.Name = DocumentName
+        self.Author = DocumentAuthor
+        self.Filename = f"{self.Name}_{"".join(self.Author.split(" "))}.asciidoc"
+        self.Data = {}
+        # The structure in this attribute (self.Data) will
+        # look like this
+        """
+        {
+            "DocumentName": "ExampleDocument",
+            "DocumentAuthor": ["Ameen272"],
+            "CreationDate": "10-8-2025 13:13 (GMT-4)",
+            "Data": {
+                "Header": {
+                    "Text": "Example Document",
+                    "Subtitle": "Example Subtitle",
+                    "Style": {
+                        "Color": "Gradient(#000000, #FFFFFF)",
+                        "TextWeight": "Bold",
+                        "TextAlign": "Center",
+                        "Font": "Title Font"
+                    },
+                ...
+                }
+            }
+        }
+        """
+
+    def ImportHeader(
+        self,
+        Text: str,
+        Subtitle: str,
+        TextStyle: dict[str, Union[str, int]],
+        SubtitleStyle: dict[str, Union[str, int]],
+    ):
+        """
+        Document.ImportHeader()         —— A method to import a header in this class
+
+        This method imports and creates a header in the document. This method has so many
+        arguments, because this includes style and color, and more stuff.
+
+        Arguments:
+            `DocumentName`: This parameter denotes the ascii document name
+            `DocumentAuthor`: This parameter denotes the author of the ascii document
+        Returns: `None`
+        """
+        self.Data["Header"] = {
+            "Text": Text,
+            "Subtitle": Subtitle,
+            "Style": {
+                "Text Color": TextStyle.get("Color", "#FFFFFF"),
+                # It also could be a gradient like this
+                # `Gradient(ColorA, ColorB)`
+                "Text Weight": TextStyle.get("Text Weight", "Normal"),
+                "Text Align": TextStyle.get("Align", "Center"),
+                "Text Font": TextStyle.get("Font", "Title"),
+                "Subtitle Color": SubtitleStyle.get("Color", "#FFFFFF"),
+                "Subtitle Weight": SubtitleStyle.get("Weight", "Normal"),
+                "Subtitle Align": SubtitleStyle.get("Align", "Center"),
+            },
+        }
