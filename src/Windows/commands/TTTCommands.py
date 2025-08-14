e5f16d19c84750fffa29c9decb48687e8d0fe4db
ameen272 <107958859+ameen272@users.noreply.github.com>
2025-08-15 02:18:51 +0300

    Update and rename tttcommands.py to TTTCommands.py

---
+++ src/Windows/commands/TTTCommands.py
@@ -0,0 +1,8 @@
+def TTTCommands():
+    """
+    TTTCommands()        -- Function The Twelly Creations.
+    """
+
+    print(" " * 19, "ttt  - The Twelly Tracker's command (Not implemented yet).")
+
+TTTCommands()
--- src/Windows/commands/tttcommands.py
+++
@@ -1,8 +0,0 @@
-def TTTCommands():
-    """
-    TTTCommands()        -- Function that prints out help related to The Twelly Creations.
-    """
-
-    print("ttt  - The Twelly Tracker's command (Not implemented yet).")
-
-TTCommands()
